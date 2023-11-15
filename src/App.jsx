5d8a6f2c115a6f2924b5572e676e0075d7db4935
Nhoni <nhmbemba@gmail.com>
2023-11-15 09:43:48 +0100

    remove Fab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
-import { Box, Stack, Typography } from '@mui/material';
-
+import "./App.css";
+import { Box, Stack, Typography, Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-
   return (
     <>
       <Typography variant="subtitle1" textTransform={"uppercase"}>
@@ -13,14 +12,26 @@ function App() {
         Plus de puissance pour plus d'efficacité
       </Typography>
       <Stack direction="row" spacing={2} paddingTop={5}>
-        <Box sx={{ width:'50%', height: 450, borderRadius: '2vw 4vw', bgcolor: "primary.main",}}></Box>
-        <Stack width='50%' spacing={2}>
-          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "primary.main",}}></Box>
-          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "primary.main",}}></Box>
+        <Box sx={{ width: '50%', height: 450, borderRadius: '2vw 4vw', bgcolor: "info.main", position: 'relative'}}>
+          <Fab aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+            <AddIcon />
+          </Fab>
+        </Box>
+        <Stack width="50%" spacing={2}>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "secondary.main", position: 'relative'}}>
+            <Fab aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+              <AddIcon />
+            </Fab>
+          </Box>
+          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "success.main", position: 'relative'}}>
+            <Fab aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+              <AddIcon />
+            </Fab>
+          </Box>
         </Stack>
       </Stack>
     </>
   );
 }
 
-export default App
+export default App;
